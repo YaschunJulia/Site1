84198055c5226b5f65901957c2c05a8180646e0b
YaschunJulia <jukiyaschun@gmail.com>
2016-08-17 11:34:01 +0300

    v 1.0

---
+++ js/main.js
@@ -0,0 +1,23 @@
+var slider = document.getElementsByClassName('slider')[0];
+
+var radios = document.getElementsByClassName('slider_check');
+
+var len = radios.length;
+
+function sliderStart(){	
+	for (var i=0; i<len; i++){
+		if (radios[i].checked){			
+			break;
+		}
+	}	
+	 (i <(len-1)) ? i++: i=0;
+	 radios[i].checked = true;
+	
+}
+
+var timer = setInterval(sliderStart, 3000);
+
+
+
+
+
